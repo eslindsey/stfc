305ed1aa1f7dff5f70b22385b08de97d0999defd
Eric Lindsey <eslindsey@gmail.com>
2023-05-23 16:38:51 -0400

    Add more headers

--- stfc.go
+++ stfc.go
@@ -18,12 +18,16 @@ func Login(username, password string) (*Session, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", "https://cdn-nv3-live.startrek.digitgaming.com/accounts/v1/accounts/login/windows", nil)
 	req.Header.Add("User-Agent",       "UnityPlayer/2020.3.18f1-digit-multiple-fixes-build (UnityWebRequest/1.0, libcurl/7.75.0-DEV)")
+	req.Header.Add("Accept",           "*/*")
+	req.Header.Add("Accept-Encoding",  "gzip, deflate")
 	req.Header.Add("X-TRANSACTION-ID", "ca98560c-e47c-4af5-b859-e35764181733")   // TODO: Randomize
 	req.Header.Add("X-PRIME-VERSION",  "1.000.31110")
 	req.Header.Add("X-Suppress-Codes", "1")
+	req.Header.Add("Content-Type",     "application/x-www-form-urlencoded")
 	req.Header.Add("X-Api-Key",        "FCX2QsbxHjSP52B")
 	req.Header.Add("X-PRIME-SYNC",     "0")
 	req.Header.Add("X-Unity-Version",  "2020.3.18f1-digit-multiple-fixes-build")
+	req.Header.Add("Connection",       "close")
 	req.Form = url.Values{
 		"auth_provider":   {""},
 		"auth_token":      {""},
