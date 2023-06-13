b8e4dbbca559566a9ff278b3ea2a9a94fdac5e43
Eric Lindsey <eslindsey@gmail.com>
2023-06-13 16:59:14 -0400

    Add notes for playfield size

--- endpoint_courses_setfleetwarpcourse.go
+++ endpoint_courses_setfleetwarpcourse.go
@@ -9,13 +9,15 @@ type CoursesSetFleetWarpCourseRequest struct {
 	TargetActionId int         `json:"target_action_id"`
 	FleetId        uint64      `json:"fleet_id"`
 	TargetNode     NodeId      `json:"target_node"`
-	TargetX        float32     `json:"target_x"`
-	TargetY        float32     `json:"target_y"`
+	TargetX        float32     `json:"target_x"`  // see TODO
+	TargetY        float32     `json:"target_y"`  // see TODO
 	TargetAction   int         `json:"target_action"`
 	ClientWarpPath []NodeId    `json:"client_warp_path"`
 	ClientWarpCost interface{} `json:"client_warp_cost"`
 	IsInstantWarp  bool        `json:"is_instant_warp"`
 }
+
+// TODO: X and Y for playfield should be constrained to a circle of radius 1100.0, but probably not in this file
 
 func (s *Session) CoursesSetFleetWarpCourse(request *CoursesSetFleetWarpCourseRequest) ([]byte, error) {
 	b, err := json.Marshal(request)
